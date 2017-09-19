f43e7bb46aee73c84f6db75c77d5af38fa7cdf36
ASOST\1401341 <dd>
2017-09-19 10:06:14 +0900

    MoveUI

--- Game/Assets/GameMain/Script/InputManager.cs
+++ Game/Assets/GameMain/Script/InputManager.cs
@@ -8,6 +8,8 @@ public class InputManager : MonoBehaviour
     //コントローラ
     [SerializeField]
     private GameObject stick, stickBase;
+    [SerializeField]
+    private GameObject leftTap, rightTap;
     //uiCamera
     [SerializeField]
     Camera uiCamera;
@@ -15,6 +17,13 @@ public class InputManager : MonoBehaviour
     public float distance = 100f;
     float len;
     float maxLen = 1.0f;
+    bool test = true;
+    string objectName;
+
+    bool tapFlag = true;
+
+    Color setColor = new Color(1,1,1,1);
+    Color resetColor = new Color(1, 1, 1, 0.5f);
     void Start()
     {
 
@@ -22,10 +31,11 @@ public class InputManager : MonoBehaviour
     
     void Update()
     {
+      //   Move();
         TapMove();
     }
-    //プレイヤーの移動処理
-    void TapMove()
+    //プレイヤーの移動処理一つ目
+    void Move()
     {
         if (Input.GetMouseButtonDown(0))
         {
@@ -59,5 +69,49 @@ public class InputManager : MonoBehaviour
         }
 
     }
-  
+    //
+    void TapMove()
+    {
+        TapRay();
+        TapUpReset();
+    }
+
+  //タップしたオブジェクトの名前を取ってくる
+    void TapRay()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = uiCamera.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+
+            if (Physics.Raycast(ray, out hit, distance))
+            {
+                objectName = hit.collider.gameObject.name;
+            }
+            if (tapFlag)
+            {
+                switch (objectName)
+                {
+                    case "Left":
+                        leftTap.GetComponent<SpriteRenderer>().color = setColor;
+                        tapFlag = false;
+                        break;
+                    case "Right":
+                        rightTap.GetComponent<SpriteRenderer>().color = setColor;
+                        tapFlag = false;
+                        break;
+                }
+            }
+        }
+    }
+    //手を離したら元に戻す
+    void TapUpReset()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            tapFlag = true;
+            leftTap.GetComponent<SpriteRenderer>().color = resetColor;
+            rightTap.GetComponent<SpriteRenderer>().color = resetColor;
+        }
+    }
 }
