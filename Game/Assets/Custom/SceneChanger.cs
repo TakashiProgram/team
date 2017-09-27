41b2372aa7f43f0d4413b78e7350853ceb23b31d
ASOST\1401346 <1>
2017-09-27 11:14:09 +0900

    拡張機能を保存するCustomフォルダ追加 CustomフォルダにSceneChangerとSceneListを追加 マスクを元にフェードインフェードアウトをするFadeShaderの追加

---
+++ Game/Assets/Custom/SceneChanger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public static class SceneChanger {
+
+	
+   public static void LoadSceneAtList(SceneNameList _listName)
+    {
+        if((int)_listName < 0)
+        {
+            Debug.LogError("_listNameの値が不正です。_listNameの値は正の値でなくてはなりません");
+            return;
+        }
+       
+        SceneManager.LoadScene((int)_listName,LoadSceneMode.Single);
+    }
+
+   public static void LoadSceneAtListAsync(SceneNameList _listName)
+    {
+        if ((int)_listName < 0)
+        {
+            
+            Debug.LogError("_listNameの値が不正です。_listNameの値は正の値でなくてはなりません");
+            return;
+        }
+        
+        SceneManager.LoadSceneAsync((int)_listName,LoadSceneMode.Single);
+    }
+}
