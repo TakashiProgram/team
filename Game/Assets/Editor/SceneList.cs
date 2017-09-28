ce3a9a13cb5c9405d5f51706e9ea4c02ae5c2c91
ASOST\1401346 <1>
2017-09-28 10:41:03 +0900

    SceneListにScenesフォルダのBuild内に格納されたフォルダを自動でBuildSettingのシーンに追加する機能追加 ScenesフォルダとBuildフォルダの追加

--- Game/Assets/Editor/SceneList.cs
+++ Game/Assets/Editor/SceneList.cs
@@ -6,10 +6,115 @@ using System.Text;
 using System.Linq;
 using UnityEngine;
 using UnityEditor;
+using UnityEditorInternal;
 
+public class BuildSettingSceneFile : AssetPostprocessor
+{
+    private const string BUILD_DIRECTORY_PATH = "Assets/Scenes/Build";
+    private const string BUILD_DIRECTORY_FIRST_PATH = "Assets/Scenes/Build/First";
+    static bool ExistsDrectryInAssets(List<string[]> assetsList,List<string> targetDirectoryNameList)
+    {
+        return assetsList
+              .Any(assets => assets
+              .Select(asset => System.IO.Path.GetDirectoryName(asset))
+              .Intersect(targetDirectoryNameList)
+              .Count() > 0);
+    }
+
+
+    private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets,string[] movedFromAssetPaths)
+    {
+        List<string[]> assetsList = new List<string[]>() {
+            importedAssets,deletedAssets,movedAssets,movedFromAssetPaths
+        };
+        List<string> targetDirectoryNameList = new List<string>()
+        {
+          BUILD_DIRECTORY_PATH,BUILD_DIRECTORY_FIRST_PATH,
+        };
+
+        //変更ファイルが指定したディレクトリ内ならビルド情報を更新する
+        if (ExistsDrectryInAssets(assetsList, targetDirectoryNameList))
+        {
+            UpdateScenesInBuild();
+            SceneList.CreateList();
+        }
+    }
+
+    //対象のファイルに格納されているものがあるかを検索します。
+    //true : 空の場合 false : 中身が存在する
+    private static bool EmptyChack(string path,string directory)
+    {
+        string[] directories = Directory.GetDirectories(path);
+        string[] files = Directory.GetFiles(path);
+        if(directories.Length + files.Length == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    [MenuItem("Tools/Update/Scenes In Build")]
+    private static void UpdateScenesInBuild()
+    {
+
+        //sceneファイルの全取得
+        List<string> pathList = new List<string>();
+        string firstScenePath = "";
+
+        foreach(var guid in AssetDatabase.FindAssets("t:Scene"))
+        {
+            string path = AssetDatabase.GUIDToAssetPath(guid);
+            string sceneName = System.IO.Path.GetFileNameWithoutExtension(path);
+
+            //指定ディレクトリ以外のシーンはスルー処理
+            if (!path.Contains(BUILD_DIRECTORY_PATH))
+            {
+                continue;
+            }
+
+            //シーン名が同名ならエラー表示
+            if (pathList.Contains(sceneName))
+            {
+                Debug.LogError(sceneName + "というシーン名が重複しています。");
+
+            }
+            //親ディレクトリがFirstなら最初のシーンに設定
+            else if(System.IO.Path.GetFileName(System.IO.Path.GetDirectoryName(path)) == "First")
+            {
+                Debug.Log(System.IO.Path.GetFileName(System.IO.Path.GetDirectoryName(path)));
+                //二つ以上入っているならエラー
+                if (!string.IsNullOrEmpty(firstScenePath))
+                {
+                    Debug.LogError("Firstディレクトリに複数シーンが設定されています。Firstディレクトリには一つのシーンしか設定できません。");
+                }
+                firstScenePath = path;
+            }
+            //パスをリストに追加する
+            else
+            {
+                pathList.Add(path);
+            }
+        }
+
+        //追加するシーンのリストの作成追加
+        List<EditorBuildSettingsScene> sceneList = new List<EditorBuildSettingsScene>();
+        Debug.Log(string.IsNullOrEmpty(firstScenePath));
+        if (!string.IsNullOrEmpty(firstScenePath))
+        {
+            Debug.Log("PushBuildSceneTo" + firstScenePath);
+            sceneList.Add(new EditorBuildSettingsScene(firstScenePath, true));
+        }
+        foreach(string path in pathList)
+        {
+            Debug.Log("AddScene");
+            sceneList.Add(new EditorBuildSettingsScene(path, true));
+        }
+        EditorBuildSettings.scenes = sceneList.ToArray();
+    }
+}
 
 public static class SceneList {
-
+ 
     //無効な文字列を管理する固定配列
     private static readonly string[] INVALUD_CHARS =
     {
@@ -23,7 +128,7 @@ public static class SceneList {
     };
 
     private const string ITEM_NAME = "Tools/Create/SceneNameList";
-    private const string PATH = "Assets/Custom/SceneName.cs";
+    private const string PATH = "Assets/SceneName.cs";
 
     private static readonly string FILENAME = Path.GetFileName(PATH);                   // ファイル名(拡張子あり)
     private static readonly string FILENAME_WITHOUT_EXTENSION = Path.GetFileNameWithoutExtension(PATH);   // ファイル名(拡張子なし)
@@ -42,7 +147,7 @@ public static class SceneList {
         }
         CreateScript();
 
-        EditorUtility.DisplayDialog(FILENAME, "作成が完了しました", "OK");
+       // EditorUtility.DisplayDialog(FILENAME, "作成が完了しました", "OK");
 
     }
 
