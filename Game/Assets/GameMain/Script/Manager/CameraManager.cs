39ad601d2ba166b2ac6838512801eb95ab372bf8
ASOST\1401341 <dd>
2017-10-16 11:15:39 +0900

    Time追加 Camera画面外作成

---
+++ Game/Assets/GameMain/Script/Manager/CameraManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+    [SerializeField]
+    private int m_PosMinX,m_PosMaxX,m_PosMinY,m_PosMaxY;
+
+    [SerializeField]
+    private GameObject player;
+
+    private float m_PosMaxZ = -4.75f;
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame8.85
+	void Update () {
+        this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, m_PosMaxZ);
+        this.transform.position = new Vector3(Mathf.Clamp(this.transform.position.x, m_PosMinX, m_PosMaxX),
+                                              Mathf.Clamp(this.transform.position.y+2, m_PosMinY, m_PosMaxY), m_PosMaxZ);
+	}
+}
---
+++ Game/Assets/GameMain/Script/Manager/TimeManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour {
+    [SerializeField]
+    private Text timeText;
+    [SerializeField]
+    private float m_Time;
+
+    int d;
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Disable();
+
+    }
+    private void Disable()
+    {
+        timeText.text = "Time: " + Mathf.FloorToInt(m_Time);
+        m_Time -= Time.deltaTime;
+       
+        
+    }
+}
