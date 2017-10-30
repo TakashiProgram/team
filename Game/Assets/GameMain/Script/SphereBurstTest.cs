77d2c45704d90c5bc365174a5a79a4bf8a4a204b
ASOST\1401349 <aa@gmail.com>
2017-10-30 10:52:34 +0900

    Shadersフォルダを作成して.shaderファイルをそこに移動しました Bubble_Burst.shaderにオブジェクトとの衝突部分から割れるような表現を追加 バブル用の球体モデルをModelフォルダに移動 バブル用テクスチャをGameMain/Material/BubbleTextureに移動 バブル制御用のSphereBurstTestをGameMain/Scriptに追加（制作中）

---
+++ Game/Assets/GameMain/Script/SphereBurstTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereBurstTest : MonoBehaviour {
+    Material _material;
+    //オブジェクトとのヒット位置（ワールド座標系）
+    Vector3 _hitPosition;
+	// Use this for initialization
+	void Start () {
+        _material = GetComponent<Renderer>().material;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Damage")
+        {
+            /*Collider.ClosestPointOnBounds(Vector3)
+             * 設定した座標に一番近いColliderオブジェクトの座標を返す
+             *
+             */
+            _hitPosition = col.ClosestPointOnBounds(this.transform.position);
+            Debug.Log(_hitPosition);
+            Vector4 hitPos = new Vector4(_hitPosition.x,_hitPosition.y,_hitPosition.z,1);
+            _material.SetVector("_HitPosition", hitPos);
+        }
+        //とりあえず何か当たったら止まるようにしとく（テスト用
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Damage")
+        {
+            foreach (ContactPoint point in col.contacts)
+            {
+                _hitPosition = point.point;
+                Debug.Log(_hitPosition);
+                Vector4 hitPos = new Vector4(_hitPosition.x, _hitPosition.y, _hitPosition.z, 1);
+                _material.SetVector("_HitPosition", hitPos);
+            }
+        }
+        //とりあえず何か当たったら止まるようにしとく（テスト用
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+}
