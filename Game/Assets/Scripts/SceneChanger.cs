01e5122acf7aa0bcbc822457b5f2e02b21587e39
ASOST\1401346 <1>
2017-11-07 10:44:21 +0900

    SceneChangerなどのスクリプトをScriptsフォルダへ移動 StageStatusをStageChangerへ名前変更 StageChanger及びSceneChangerでフェードインに対応した非同期ロードを実装

---
+++ Game/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public static class SceneChanger {
+
+	
+   public static void LoadSceneAtList(SceneNameList _listName)
+    {
+        if((int)_listName < 0)
+        {
+            Debug.LogError("_listNameの値が不正です。_listNameの値は正の値でなくてはなりません");
+            return;
+        }
+       
+        SceneManager.LoadScene((int)_listName,LoadSceneMode.Single);
+    }
+
+  public static IEnumerator LoadSceneAtListAsync(SceneNameList _listName)
+    {
+        Debug.Log("LoadScene To :" + _listName.ToString());
+        if ((int)_listName < 0)
+        {
+            yield break;
+        }
+      
+        Fader fadeTarget =  GameObject.Find("FadeCamera").GetComponent<Fader>();
+        if (fadeTarget != null)
+        {
+            fadeTarget.FadeIn();
+            yield return new WaitForSeconds(fadeTarget.GetFadeTime());
+        }
+
+
+        AsyncOperation asyncData =  SceneManager.LoadSceneAsync((int)_listName,LoadSceneMode.Single);
+        asyncData.allowSceneActivation = false;
+
+        while(asyncData.progress < 0.9f)
+        {
+            
+            Debug.Log((int)((asyncData.progress / 0.9f) * 100) +"%");
+            yield return new WaitForEndOfFrame();
+        }
+        Debug.Log("Secene Loaded");
+        asyncData.allowSceneActivation = true;
+    }
+}
