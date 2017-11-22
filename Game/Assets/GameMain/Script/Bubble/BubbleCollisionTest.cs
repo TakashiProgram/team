dd6e5aa1bc9c12d42fa89e48e7044c54f1c14dfa
ASOST\1401349 <aa@gmail.com>
2017-11-22 14:57:16 +0900

    テスト用のBubbleCollisionTest追加　後で消す

---
+++ Game/Assets/GameMain/Script/Bubble/BubbleCollisionTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleCollisionTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag != "Player")
+        {
+            Debug.Log("Burst(col)を呼ぶ");
+            gameObject.GetComponent<BubbleController>().Burst(col);
+        }
+    }
+}
