c0b96e02cc7f812998cb117bf510e4bc68ee6807
ASOST\1401343 <1401343>
2017-12-15 12:30:17 +0900

    Titleシーンでフェード処理中に入力を受け付けないように変更,bubleFactoryのscriptをScriptsフォルダに移動

---
+++ Game/Assets/Scripts/Title/TitleManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+    bool changeFlag;  // シーン遷移可能かどうか
+    
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GameObject fade = GameObject.Find("FadeCamera");     // 
+        
+        if (fade.GetComponent<Fader>().IsFadeEnd())
+        {
+            
+            // 画面クリック/タップで遷移
+            if (Input.GetMouseButtonDown(0))
+            {
+                SceneChanger.LoadSceneAtListAsync(SceneNameList.GameMain);
+            }
+        }
+    }
+}
