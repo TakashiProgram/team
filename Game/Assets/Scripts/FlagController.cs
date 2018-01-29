873706670cc5f97d95ab7a89cb03c456df974162
ASOST\1401343 <1401343@st.asojuku.ac.jp>
2018-01-29 13:53:20 +0900

    FlagがPlayerに接触したときにアニメを開始させるScriptを追加

---
+++ Game/Assets/Scripts/FlagController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagController : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter(Collider _coll)
+    {
+        Debug.Log(_coll.tag);
+        if (_coll.tag == "Player")
+        {
+            this.GetComponent<Animator>().SetTrigger("StandTrigger");
+        }
+    }
+}
